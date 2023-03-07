46c094b3e15b50047669c2236d9ce620591d0de5
hefekuchen <tsotne251@gmail.com>
2023-03-07 09:48:39 +0100

    restructured notes folders

--- part3/notesBackend/utils/config.js
+++ part3/notesBackend/utils/config.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT
 
 const MONGODB_URI = process.env.NODE_ENV === 'test'
   ? process.env.TEST_MONGODB_URI
-  : process.env.MONGODB_URI_NOTEAPP
+  : process.env.MONGODB_URI_NOTES
 
 module.exports = {
   MONGODB_URI,
