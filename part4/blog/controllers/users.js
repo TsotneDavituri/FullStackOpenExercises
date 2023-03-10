3133e948c8619d88051f22b8a7563953aa0290a0
Tsotne Davituri <tsotne250250@gmail.com>
2023-03-10 10:58:28 +0100

    completed exercises 4.15-4.19

--- part4/blog/controllers/users.js
+++ part4/blog/controllers/users.js
@@ -2,8 +2,18 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({}).populate('blogs')
+  response.json(users)
+})
+
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
+
+  if (password.length < 3) {
+    return response.status(400).json({error: 'password should be 3 or more characters'})
+  }
 
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
@@ -17,11 +27,6 @@ usersRouter.post('/', async (request, response) => {
   const savedUser = await user.save()
 
   response.status(201).json(savedUser)
-})
-
-usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
-  response.json(users)
 })
 
 module.exports = usersRouter
--- part4/blog/models/user.js
+++ part4/blog/models/user.js
@@ -5,10 +5,18 @@ const userSchema = mongoose.Schema({
   username: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
+    minLength: 3
   },
   name: String,
   passwordHash: String,
+  blogs:
+  [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Blog'
+    }
+  ]
 })
 
 userSchema.plugin(uniqueValidator)
@@ -22,6 +30,5 @@ userSchema.set('toJSON', {
   }
 })
 
-const User = mongoose.model('User', userSchema)
+module.exports = mongoose.model('User', userSchema)
 
-module.exports = User
