f47f0c90d35fbe570af1552752847b5cb0dd0c95
Tsotne <TsotneDavituri@users.noreply.github.com>
2023-03-15 16:11:49 +0100

    refactored handleLike function

--- part4/bloglist-frontend/src/components/Blog.js
+++ part4/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,7 @@
 import Togglable from "./Togglable"
 import { useState } from "react"
 
-const Blog = ({blog, handleLikeIncrease, blogService, user}) => {
+const Blog = ({blog, blogService, user, blogs, setBlogs}) => {
   const [likes, setLikes] = useState(blog.likes)
 
   const blogStyle = {
@@ -12,11 +12,13 @@ const Blog = ({blog, handleLikeIncrease, blogService, user}) => {
     marginBottom: 5
   }
 
-  const handleLikeClick = async () => {
+  const handleLike = async () => {
     const updatedBlog = { ...blog, likes: likes + 1 }
-    await handleLikeIncrease(blog.id, updatedBlog, blogService)
-    setLikes(updatedBlog.likes)
+    const returnedBlog = await blogService.update(blog.id, updatedBlog)
+    setLikes(returnedBlog.likes)
+    setBlogs(blogs.map(blog => blog.id === returnedBlog.id ? returnedBlog : blog))
   }
+
 
   return (
     <div style={blogStyle}>
@@ -30,10 +32,13 @@ const Blog = ({blog, handleLikeIncrease, blogService, user}) => {
         </div>
         <div>
           {blog.likes} 
-          <button onClick={handleLikeClick}>like</button>
+          <button onClick={handleLike}>like</button>
         </div>
         <div>
           {user.name}
+        </div>
+        <div>
+          <button>remove</button>
         </div>
       </Togglable>
     </div>
